4061f7ec9d0fe63a2ca7368ef06c850d39101888
Nebula S. Nova <jasontheone111@outlook.com>
2025-07-27 15:05:08 -0400

    organization

--- data/global.hx
+++ data/global.hx
@@ -15,3 +15,42 @@ function getStateName():Void {
 	var split:Array<String> = Type.getClassName(Type.getClass(FlxG.game._requestedState)).split('.');
 	return split[split.length - 1];
 }
+
+function blendMode(name:String) {
+	return [
+		"add",
+		"alpha",
+		"darken",
+		"difference",
+		"erase",
+		"hardlight",
+		"invert",
+		"layer",
+		"lighten",
+		"muiltiply",
+		"normal",
+		"overlay",
+		"screen",
+		"shader",
+		"subtract"
+	].indexOf(name.toLowerCase());
+}
+
+static var BlendMode = {
+	fromString: blendMode,
+	ADD: 0,
+	ALPHA: 1,
+	DARKEN: 2,
+	DIFFERENCE: 3,
+	ERASE: 4,
+	HARDLIGHT: 5,
+	INVERT: 6,
+	LAYER: 7,
+	LIGHTEN: 8,
+	MULTIPLY: 9,
+	NORMAL: 10,
+	OVERLAY: 11,
+	SCREEN: 12,
+	SHADER: 13,
+	SUBTRACT: 14
+}
