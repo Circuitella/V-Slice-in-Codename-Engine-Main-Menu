3d2f8ef712582be8331732dbec4fac1f4ef84469
Circuitella <116322307+Circuitella@users.noreply.github.com>
2025-08-07 13:06:14 -0400

    Update MainMenuState.hx

--- data/states/MainMenuState.hx
+++ data/states/MainMenuState.hx
@@ -1,3 +1,37 @@
+import flixel.FlxObject;
+
+
+function postCreate() {
+    for(bg in [bg,magenta]){
+    bg.scrollFactor.x = 0; 
+    bg.scrollFactor.y = 0.17;
+    bg.setGraphicSize(Std.int(FlxG.width * 1.2));
+    bg.updateHitbox();
+    bg.screenCenter();
+    }
+    camFollow = new FlxObject(camFollow, 0, 1, 1);
+
+    var spacing = 160;
+    var top = (FlxG.height - (spacing * (menuItems.length - 1))) / 2;
+    for (i in 0...menuItems.length)
+    {
+      var menuItem = menuItems.members[i];
+      menuItem.x = FlxG.width / 2;
+      menuItem.y = top + spacing * i;
+      menuItem.scrollFactor.x = #if !mobile 0.0 #else 0.4 #end; // we want a lil scroll on mobile, for the cute gyro effect
+      // This one affects how much the menu items move when you scroll between them.
+      menuItem.scrollFactor.y = 0.4;
+
+      if (i == 1)
+      {
+        camFollow.setPosition(menuItem.getGraphicMidpoint().x, menuItem.getGraphicMidpoint().y);
+      }
+    }
+    camFollow.y = bg.getGraphicMidpoint().y;
+    FlxG.camera.follow(camFollow, null, 0.06);
+
+}
+
 
 function onSelectItem(event) {
     if (event.name == "freeplay") {
